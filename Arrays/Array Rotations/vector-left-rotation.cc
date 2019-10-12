d82465e5c0f2e854da86fd68dac44559df80421d
abhishekarya1 <37147545+abhishekarya1@users.noreply.github.com>
2019-10-12 13:53:52 +0530

    vector array left rotation code

---
+++ Arrays/Array Rotations/vector-left-rotation.cc
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+	std::vector<int> v;
+
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(3);
+	v.push_back(4);
+	v.push_back(5);
+
+	int k = 2;
+
+	for (int i = 0; i < k; ++i)
+	{
+		v.push_back(v[0]);
+		v.erase(v.begin());
+	}
+
+
+	for (std::vector<int>::iterator i = v.begin(); i != v.end(); ++i)
+	{
+		cout << *i << " ";
+	}
+}
