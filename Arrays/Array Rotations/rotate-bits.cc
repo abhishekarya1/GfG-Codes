d755021f285ba4971770f8d4d8001a8c87db1192
abhishekarya1 <37147545+abhishekarya1@users.noreply.github.com>
2019-10-12 16:05:18 +0530

    array code#2

--- Arrays/Array Rotations/rotate-bits.cc
+++ Arrays/Array Rotations/rotate-bits.cc
@@ -6,12 +6,12 @@ using namespace std;
 
 int left_rotate_bits(int n, unsigned int k)
 {
-	return n << k | (n >> (INT_BITS - k));
+	return n << k | (n >> (INT_BITS - k));		// rotate by complement (out of 32) in opposite direction and OR them
 }
 
-int right_rotate_bits(int n, int k)
+int right_rotate_bits(int n, int k)				
 {
-	return n >> k | (n << (INT_BITS - k));
+	return n >> k | (n << (INT_BITS - k));		// rotate by complement (out of 32) in opposite direction and OR them
 }
 
 int main()
