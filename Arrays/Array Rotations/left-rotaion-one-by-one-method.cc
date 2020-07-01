e7973843c125adeb7a0e043a540d23e226e8d80a
abhishekarya1 <37147545+abhishekarya1@users.noreply.github.com>
2020-07-01 18:08:25 +0530

    Create left-rotaion-one-by-one-method.cc

---
+++ Arrays/Array Rotations/left-rotaion-one-by-one-method.cc
@@ -0,0 +1,36 @@
+// Left Rotaion by k postions (one-by-one method)
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int arr[] = {1, 2, 3, 4, 5};
+
+	int size = sizeof(arr) / sizeof(arr[0]);
+
+	int k;
+	cin >> k;
+
+	//do it k times	-- O(k)
+	for (int i = 0; i < k; ++i)
+	{
+		//extract element from the start
+		int e = arr[i];
+
+		//shift rest of the elements -- O(n)
+		for (int j = 0; j < size - 1; ++j)
+		{
+			arr[j] = arr[j + 1];
+		}
+
+		//insert extracted element back at the end
+		arr[size - 1] = e;
+	}	
+
+	//print array
+	for (int i = 0; i < size; ++i)
+	{
+		cout << arr[i] << " ";
+	}
+}
