c591b163ccc0b570659f1879a869daa7461c56a8
abhishekarya1 <37147545+abhishekarya1@users.noreply.github.com>
2020-07-15 15:10:19 +0530

    Update palindromic_prime.cc

--- Mathematical/Prime Numbers and Sieves/palindromic_prime.cc
+++ Mathematical/Prime Numbers and Sieves/palindromic_prime.cc
@@ -1,44 +1,50 @@
-// Sophie-Germain Prime = If p is prime and 2p+1 is also a prime, then p is a sophie-germain prime.
+//Palindromic Prime = A number whose palindrome is also a prime
 
-// Link: https://www.geeksforgeeks.org/sophie-germain-prime/
+// Link: https://www.geeksforgeeks.org/palindromic-primes/
 
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+
 using namespace std;
 
-void sieve(int n, int* s)
+bool isPal(int n)
 {
-	s[0] = 0;
-	s[1] = 0;
+	string num1 = to_string(n);
+	string num = num1;
+	reverse(num.begin(), num.end());
 
-	for (int i = 2; i * i <= n; ++i)
+	if (num == num1)
+		return true;
+	else return false;
+}
+
+void sieve(int n, vector<int> &s)
+{
+	s[0] = s[1] = 0;
+	for (int i = 2; i * i <= n; i++)
 	{
 		if (s[i])
 		{
-			for (int j = i * i; j <= n; ++j)
-			{
+			for (int j = i * i; j <= n; j += i)
 				s[j] = 0;
-			}
 		}
 	}
 }
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
 	int n;
 	cin >> n;
 
-	int s[n + 1];
-	memset(s, 1, sizeof(s));
+	vector<int> s(n + 1, 1);
 	sieve(n, s);
 
-	for (int i = 2; i < (n - 1) / 2; ++i)
-	{
-		if (s[i] == 1 && s[2 * i + 1] == 1)
-			cout << i << " ";
-	}
+	for (int i = 0; i < s.size(); i++)
+		if (s[i] == 1)
+			if (isPal(i))
+				cout << i << " ";
 
 	return 0;
 }
